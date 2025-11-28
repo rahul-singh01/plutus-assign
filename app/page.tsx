cfa3be99d68cd912a7dd7ff8f6a723dbe00aaaf7
Rahul Singh <rahulsinghrajput6090@gmail.com>
2025-11-28 15:35:34 +0530

    Added: All component on root page /.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import WhyMoonEX from "@/components/WhyMoonEX";
+import FAQ from "@/components/FAQ";
+import Footer from "@/components/Footer";
+import Features from "@/components/Features";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#051422] text-white overflow-hidden selection:bg-primary selection:text-black">
+    <main className="min-h-screen bg-background text-white overflow-x-hidden selection:bg-primary selection:text-black">
       <Navbar />
       <Hero />
+      <WhyMoonEX />
+      <Features />
+      <FAQ />
+      <Footer />
     </main>
   );
 }
